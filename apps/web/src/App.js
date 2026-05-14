df273df3cd2383d8df304f53708c12286828be68
malsaslamcraft97 <aslam@incubyte.co>
2026-05-14 16:12:41 +0530

    init: scaffold and setup the initials with turborepo - React + Vite + TS === Frontend - NodeJS + NestJS === Backend (API)

---
+++ apps/web/src/App.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export default function App() {
+    return (_jsx("main", { children: _jsx("h1", { children: "My Task Board" }) }));
+}
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main>
+      <h1>My Task Board</h1>
+    </main>
+  );
+}
